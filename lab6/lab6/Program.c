88dfd6d8bd889b07d53f28d10ac44272ea1fe9ae
PH68526 <duyhung6122007@gmail.com>
2025-11-27 11:45:20 +0700

    cap nhap lab6bai1

---
+++ lab6/lab6/Program.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    int mang[100];   // khai báo sau khi nhập n
+
+    // Nhập dữ liệu cho mảng
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i);
+        scanf("%d", &mang[i]);
+    }
+
+    float tong = 0;
+    int count = 0;
+
+    // Duyệt mảng, tìm số chia hết cho 3
+    for (int i = 0; i < n; i++) {
+        if (mang[i] % 3 == 0) {
+            tong += mang[i];  // cộng vào tổng
+            count++;          // tăng biến đếm
+        }
+    }
+
+    if (count > 0) {
+        float tb = tong / count;
+        printf("Gia tri trung binh cac so chia het cho 3 = %.2f\n", tb);
+    }
+    else {
+        printf("Khong co phan tu nao chia het cho 3!\n");
+    }
+
+    return 0;
+}
